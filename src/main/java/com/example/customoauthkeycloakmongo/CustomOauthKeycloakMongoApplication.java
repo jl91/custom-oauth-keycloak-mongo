16dbe08213e04a4034004e285c40478e1736c6d7
John lennon de melo fernandes <jlfernandes91@hotmail.com>
2022-11-09 21:51:49 -0300

    initial commit

---
+++ src/main/java/com/example/customoauthkeycloakmongo/CustomOauthKeycloakMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.customoauthkeycloakmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomOauthKeycloakMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomOauthKeycloakMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/customoauthkeycloakmongo/CustomOauthKeycloakMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customoauthkeycloakmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomOauthKeycloakMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
