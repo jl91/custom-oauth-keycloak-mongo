50d6b699357159c984ee36308d5cc1a62b88be5c
John lennon de melo fernandes <jlfernandes91@hotmail.com>
2022-11-09 21:55:11 -0300

    padronizando nome da startup da aplicação

--- src/main/java/com/example/customoauthkeycloakmongo/Application.java
+++ src/main/java/com/example/customoauthkeycloakmongo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomOauthKeycloakMongoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(CustomOauthKeycloakMongoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
